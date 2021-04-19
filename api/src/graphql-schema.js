873ba6af313e83d1faa0c148308f17524a08bef7
Jaksa Malisic <jaksa.malisic@gmail.com>
2021-04-19 15:35:57 +0200

    Put schema first in typeDefs

--- api/src/graphql-schema.js
+++ api/src/graphql-schema.js
@@ -1,14 +1,20 @@
 import fs from 'fs'
 import glob from 'glob'
+import path from 'path'
+
+const schemaPath =
+  process.env.GRAPHQL_SCHEMA || path.join(__dirname, 'schema.graphql')
+const readFile = (path) => fs.readFileSync(path).toString('utf-8')
 
 export const getTypeDefs = (callback) => {
   glob(`${__dirname}/**/*.graphql`, (error, paths) => {
     if (error) throw error
-    const typeDefs = paths
-      .map((filePath) => {
-        return fs.readFileSync(filePath).toString('utf-8')
-      })
-      .join('\n')
+    const schema = readFile(schemaPath)
+    const extensions = paths
+      .filter((filePath) => filePath !== schemaPath)
+      .map(readFile)
+    const typeDefs = [schema, ...extensions].join('\n')
+    console.log(typeDefs)
     callback(typeDefs)
   })
 }
